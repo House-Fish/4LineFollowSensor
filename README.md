03a2114c31374d8db053fade7ae31c7aadca4b8d
Hiwonder <44456375+Hiwonder@users.noreply.github.com>
2020-05-30 12:11:10 +0800

    Blocks for Hiwonder 4 path line follow sensor

---
+++ README.md
@@ -0,0 +1,14 @@
+# _4line_follow_sensor
+ Blocks for Hiwonder 4 path line follow sensor
+## License
+
+MIT
+
+## Supported targets
+
+* for PXT/_4line_follow_sensor
+(The metadata above is needed for package search.)
+
+```package
+4LineFollowSensor=github:Hiwonder/4LineFollowSensor
+```
---
+++ main.ts
@@ -0,0 +1,87 @@
+/*
+ _4line_follow_sensor package
+*/
+//% weight=10 icon="\uf7a5" color=#2896ff
+namespace _4line_follow_sensor {
+    export enum LineFollowerSensors {
+        //% block="S1"
+        S1,
+        //% block="S2"
+        S2,
+        //% block="S3"
+        S3,
+        //% block="S4"
+        S4
+    }
+
+    export enum LineColor {
+        //% block="Black"
+        Black,
+        //% block="White"
+        White
+    }	
+
+    const LINE_FOLLOWER_I2C_ADDR = 0x78   
+    
+    //% weight=96 blockId=line_followers block="4 Line follower %lineFollowerSensor at port 4 (P19:SCL,P20:SDA) in %lineColor ?"
+    //% inlineInputMode=inline
+    export function line_followers(lineFollowerSensor: LineFollowerSensors, lineColor: LineColor): boolean {
+        pins.i2cWriteNumber(LINE_FOLLOWER_I2C_ADDR, 1, NumberFormat.UInt8BE);	    
+	    let data = pins.i2cReadNumber(LINE_FOLLOWER_I2C_ADDR, NumberFormat.UInt8BE);
+        let status = false;
+        switch (lineFollowerSensor) {
+            case LineFollowerSensors.S1:
+                if (data & 0x01) {
+                    if (lineColor == LineColor.Black) {
+                        status = true;
+                    }
+                }
+                else {
+                    if (lineColor == LineColor.White) {
+                        status = true;
+                    }
+                }
+                break;
+
+            case LineFollowerSensors.S2:
+                if (data & 0x02) {
+                    if (lineColor == LineColor.Black) {
+                        status = true;
+                    }
+                }
+                else {
+                    if (lineColor == LineColor.White) {
+                        status = true;
+                    }
+                }
+                break;
+
+            case LineFollowerSensors.S3:
+                if (data & 0x04) {
+                    if (lineColor == LineColor.Black) {
+                        status = true;
+                    }
+                }
+                else {
+                    if (lineColor == LineColor.White) {
+                        status = true;
+                    }
+                }
+                break;
+
+            case LineFollowerSensors.S4:
+                if (data & 0x08) {
+                    if (lineColor == LineColor.Black) {
+                        status = true;
+                    }
+                }
+                else {
+                    if (lineColor == LineColor.White) {
+                        status = true;
+                    }
+                }
+                break;
+        }
+        return status;
+    }
+}
